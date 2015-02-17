9e208042ede51f7600eafaf39b7de47fc1581a13
macdja38 <jakeincanada@icloud.com>
2015-02-17 13:45:53 -0500

    Added more shapes and added a prompt to request another shape or exit.

--- ScalableShapes/src/scalableShapes/ScalableShapes.java
+++ ScalableShapes/src/scalableShapes/ScalableShapes.java
@@ -1,11 +1,8 @@
-/**
- * 
- */
 package scalableShapes;
 
 /**
+ * Class for drawing a shape and support code
  * @author macdja38
- *
  */
 public class ScalableShapes {
 	public String nCharacters(int n, char characters) {
@@ -15,9 +12,17 @@ public class ScalableShapes {
 		}
 		return returnString;
 	}
+	/**
+	 * 
+	 * @param shape		shape to draw, can be 1-6 for triangle, alternate triangle, pyramid, square, hollow square and rectangle respectively
+	 * @param height	height to draw the shape
+	 * @param character character the shape will be composed of
+	 * @return shape is drawn to console
+	 * @author macdja38
+	 */
 	public void Draw(int shape, int height, char character) {
 		switch (shape) {
-		case 1:
+		case 1: //triangle
 			for(int i=1; i<=height; i++) {
 				System.out.println(nCharacters(i,character));
 			}
@@ -52,6 +57,26 @@ public class ScalableShapes {
 		case 6: //rectangle
 			for(int i=1; i<=height; i++) {
 				System.out.println(nCharacters(height*2, character));
+			}
+			break;
+		case 7:
+			for(int i=1; i<=height/2+1; i++) {
+				System.out.print(nCharacters((height-(2*(i-1))),' '));
+				System.out.println(nCharacters((4*i)-3,character));
+			}
+			for(int i=height/2; i>0; i--) {
+				System.out.print(nCharacters(height-2*(i-1),' '));
+				System.out.println(nCharacters((4*i)-3,character));
+			}
+			break;
+		case 8:
+			for(int i=1; i<=height/2+1; i++) {
+				System.out.print(nCharacters(height-i,' '));
+				System.out.println(nCharacters((4*i)-3,character));
+			}
+			for(int i=height/2; i>0; i--) {
+				System.out.print(nCharacters(height-i,' '));
+				System.out.println(nCharacters((4*i)-3,character));
 			}
 			break;
 		}
--- ScalableShapes/src/scalableShapes/ScalableShapesTest.java
+++ ScalableShapes/src/scalableShapes/ScalableShapesTest.java
@@ -4,6 +4,7 @@
 package scalableShapes;
 
 /**
+ * code to test ScalableShapes code, requests filtered input from the user and passes ScalableShapes known valid parameters
  * @author macdja38
  *
  */
@@ -13,17 +14,15 @@ public class ScalableShapesTest {
 		int shape; //shape the user would like
 		int height; //height of the shape
 		while(true) {
-			System.out.println("Please input a 1-5 based on the shape you would like:"); 
-			shape = Keyboard.getInteger(1,5);
-			System.out.println("Please input the height:");
-			height = Keyboard.getInteger();
+			System.out.println("Please input the shape you would like: [1-8]"); 
+			shape = Keyboard.getInteger(1,8);
+			System.out.println("Please input the height: [1-100]");
+			height = Keyboard.getInteger(1,100);
 			System.out.println("Please input a character:");
 			character = Keyboard.getCharacter();
 			new ScalableShapes().Draw(shape, height, character);
-			break;
-		}
-		while(true) {
-			if (Keyboard.getBoolean() == true) {
+			System.out.println("Would you like another shape? [true/false]");
+			if (Keyboard.getBoolean() == false) {
 				break;
 			}
 		}
